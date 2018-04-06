a9deb767b33317a1fdcb0595ee7310e64a9d466c
Arianna Avanzini <avanzini.arianna@gmail.com>
2018-04-06 23:39:13 +0200

    Porductinize synergification of ze impacto

--- weddingws.py
+++ weddingws.py
@@ -1,38 +1,45 @@
 import os
 from flask import Flask
 from flask import request, send_from_directory
-app = Flask(__name__)
 
+DIR = '.'
+
+app = Flask(__name__)
 
 class InternalError(Exception):
     pass
 
+send = lambda basename: send_from_directory(DIR, basename)
 
-website_directory = '.'
-allowed_files = [name for name in os.listdir(website_directory) if name.endswith('.html')]
-if (
-    'index.html' not in allowed_files
-    or 'accueil.html' not in allowed_files
-    or 'indice.html' not in allowed_files):
-    raise InternalError('Index files not found in website directory.')
-
+indexes = ('en', 'index.html'), ('fr', 'accueil.html'), ('it', 'indice.html')
 
 @app.route("/")
 def root():
-    try:
-      language = request.headers.get('Accept-Language')
-    except KeyError:
-      language = 'en'
-    if language.startswith('it'):
-      return send_from_directory(website_directory, 'indice.html')
-    elif language.startswith('fr'):
-      return send_from_directory(website_directory, 'accueil.html')
-    return send_from_directory(website_directory, 'index.html')
-
+    language = request.headers.get('Accept-Language', 'en')
+    for lan, filename in indexes:
+      if language.startswith(lan):
+         return send(filename)
+    return send('index.html')
 
 @app.route('/<filename>')
 def page(filename):
-    if filename in allowed_files:
-        return send_from_directory(website_directory, filename)
-    else:
+    try:
+        return send(filename)
+    except IOError as e:
+        print e
         return 'Not found.', 404
+
+allowed_dirs = ('css', 'about', 'jekyll')
+    
+@app.route('/<dir>/<filename>')
+def subpage(dir, filename):
+    if dir not in allowed_dirs:
+        return 'Not found.', 404
+    try:
+        return send_from_directory(dir, filename)
+    except IOError as e:
+        print e
+        return 'Not found.', 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
